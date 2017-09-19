775af5b9b47c7e9e6922b994f792fe56339446c7
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-09-19 06:04:20 -0600

    Properly iterate over the mesh faces (subdivided plane was indexing the faces weirdly on behalf of an algorithm) and align texture as such, refactor a bunch of things

--- create_cube.py
+++ create_cube.py
@@ -2,14 +2,12 @@ import bpy
 import bmesh
 
 from mathutils import Vector, Matrix
-import math
-from random import randint
+from math import cos, sin, radians
 
 from PIL import Image
 
-
 # color data
-tex = Image.open('/home/jackhasakeyboard/py/mc/blocks/diamond_ore.png')
+tex = Image.open('/home/jackhasakeyboard/py/minecraft-bumpmap/o/diamond_ore.png')
     
 val = []
 floor = 255
@@ -25,24 +23,30 @@ for x in range(tex.size[0]):
 
 def create_plane(i):
     # init
-    bpy.ops.mesh.primitive_plane_add(radius = 8, location = (0, 0, 0))
+    bpy.ops.mesh.primitive_grid_add(x_subdivisions = 17, y_subdivisions = 17, radius = 8, location = (0, 0, 0))
 
     ob = bpy.context.object 
     me = ob.data
 
-
-    # apply material
-    bpy.ops.mesh.uv_texture_add() # unwraps, seams are irrelevant for square texture
-
     bpy.ops.object.mode_set(mode = 'EDIT')
 
+    bm = bmesh.from_edit_mesh(me)
+    
+    # apply_texture
+    bpy.ops.uv.unwrap()
     bpy.data.screens['UV Editing'].areas[1].spaces[0].image = bpy.data.images['diamond_ore.png'] # texture must be applied in edit mode
+       
+    # rotate, currently off kilter by 90 deg
+    prev_area = bpy.context.area.type
+    bpy.context.area.type = 'IMAGE_EDITOR'
 
-    bpy.ops.mesh.subdivide(number_cuts = 15)
+    bpy.ops.transform.rotate(value = radians(90))
+
+    bpy.context.area.type = prev_area
 
     bpy.ops.object.mode_set(mode = 'OBJECT')
-
-
+    
+    
     # extrude
     detail = bmesh.new()
     detail.from_mesh(me)
@@ -52,7 +56,7 @@ def create_plane(i):
         bmesh.ops.translate(detail, vec = (0, 0, (val[f] - floor) / 400), verts = r['faces'][0].verts)
 
     detail.to_mesh(me)
- 
+    
     
     # translate
     r = 0 if i == 0 else 1
@@ -61,25 +65,26 @@ def create_plane(i):
         pos = (0, 0, 8 * -r)
 
     if i == 1:
-        pos = (0, 8, 8 * -r)
+        pos = (8, 0, 8 * -r)
 
     if i == 2:
-        pos = (-8, 0, 8 * -r)
-
-    if i == 3:
         pos = (0, -8, 8 * -r)
 
+    if i == 3:
+        pos = (-8, 0, 8 * -r)
+
     if i == 4:
-        pos = (8, 0, 8 * -r)
+        pos = (0, 8, 8 * -r)
     
-    bpy.context.active_object.matrix_world *= Matrix.Rotation(math.radians(90) * r, 4, 'X')
-    bpy.context.active_object.matrix_world *= Matrix.Rotation(math.radians(90) * (i + 1) * r, 4, 'Y')
+    bpy.context.active_object.matrix_world *= Matrix.Rotation(radians(90) * r, 4, 'Y')
+    bpy.context.active_object.matrix_world *= Matrix.Rotation(radians(90) * (i - 1) * r, 4, 'X')
     bpy.context.active_object.location = pos
- 
+
     
 def create_cube():
     for i in range(5):
-      create_plane(i)
+        create_plane(i)
+      
       
     # thickness
     bpy.ops.mesh.primitive_cube_add(location = (0, 0, -8), radius = 8)
